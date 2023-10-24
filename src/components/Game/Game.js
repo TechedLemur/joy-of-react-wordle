0eac0ec624bf379b8ad1d9f1fb8cf08a72016053
Jostein Hjorland Tysse <jostein.h.tysse@gmail.com>
2023-10-24 10:44:36 +0200

    Add reset button

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -4,20 +4,27 @@ import { sample } from '../../utils';
 import { WORDS } from '../../data';
 import GuessInput from '../GuessInput/GuessInput';
 import Guess from '../Guess/Guess';
-import {NUM_OF_GUESSES_ALLOWED} from "../../constants"
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants"
 import Banner from '../Banner/Banner';
 import { checkGuess } from '../../game-helpers';
-// Pick a random word on every pageload.
-const answer = sample(WORDS);
-// To make debugging easier, we'll log the solution in the console.
-console.info({ answer });
 
 function Game() {
+  // Pick a random word on every pageload.
+  const [answer, setAnswer] = React.useState(sample(WORDS));
+  // To make debugging easier, we'll log the solution in the console.
+  console.info({ answer });
 
   const [guesses, setGuesses] = React.useState(Array(NUM_OF_GUESSES_ALLOWED).fill(""));
   const [guessIndex, setGuessIndex] = React.useState(0);
   const [gameState, setGameState] = React.useState("playing");
 
+
+  const resetGame = () => {
+    setGuesses(Array(NUM_OF_GUESSES_ALLOWED).fill(""));
+    setGuessIndex(0);
+    setGameState("playing");
+    setAnswer(sample(WORDS));
+  }
 
 
   const addGuess = (guess) => {
@@ -45,14 +52,21 @@ function Game() {
     <div className='guess-results'>
       {guesses.map((guess, index) => (
         <div key={index} className='guess-result'>
-          <Guess guess={guess} answer={answer}/>
+          <Guess guess={guess} answer={answer} />
         </div>
       ))}
     </div>
-
+    {gameState !== "playing" && (
+      <button onClick={resetGame} className="button-82-pushable" role="button">
+        <span className="button-82-shadow"></span>
+        <span className="button-82-edge"></span>
+        <span className="button-82-front text">
+          Reset Game
+        </span>
+      </button>
+    )}
     <GuessInput addGuess={addGuess}></GuessInput>
-    <Banner gameState={gameState} answer={answer} guessNumber={guessIndex}/>
-
+    <Banner gameState={gameState} answer={answer} guessNumber={guessIndex} />
   </>;
 }
 
